8b7257328f4469a74334e3c9d490e7242f780798
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 15:45:36 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//D4.1
+            int a, iloraz;
+            try
+            {
+                Console.Write("Tell me natural number a=");
+                a = Convert.ToInt32(Console.ReadLine());
+                iloraz = 1000 / a;
+                Console.WriteLine("Modulo for: 1000/{0}={1}", a, iloraz);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("You tried to divide by 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
